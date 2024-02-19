3d0e877183c6eb5a4e761e81f73e636b97c7267c
Twayfrancis <tawaifrancis@gmail.com>
2024-02-19 21:07:45 +0300

    section for lisiting jobs that are available on site

--- index.php
+++ index.php
@@ -99,3 +99,41 @@
         </div>
       </div>
     </section>
+
+    <section class="site-section">
+      <div class="container">
+
+        <!-- <div class="row mb-5 justify-content-center">
+          <div class="col-md-7 text-center">
+            <h2 class="section-title mb-2">43,167 Job Listed</h2>
+          </div>
+        </div> -->
+        
+        <ul class="job-listings mb-5">
+          <?php foreach($jobs as $job) : ?>
+            <li class="job-listing d-block d-sm-flex pb-3 pb-sm-0 align-items-center">
+              <a href="jobs/job-single.php?id=<?php echo $job->id; ?>"></a>
+              <div class="job-listing-logo">
+                <img src="users/user-images/<?php echo $job->company_image; ?>" alt="<?php echo $job->company_image; ?>" class="img-fluid">
+              </div>
+
+              <div class="job-listing-about d-sm-flex custom-width w-100 justify-content-between mx-4">
+                <div class="job-listing-position custom-width w-50 mb-3 mb-sm-0">
+                  <h2><?php echo $job->job_title; ?></h2>
+                  <strong><?php echo $job->company_name; ?></strong>
+                </div>
+                <div class="job-listing-location mb-3 mb-sm-0 custom-width w-25">
+                  <span class="icon-room"></span> <?php echo $job->job_region; ?>
+                </div>
+                <div class="job-listing-meta">
+                <span class="badge badge-<?php if($job->job_type == 'Part Time') { echo 'danger'; } else { echo 'success'; } ?>"><?php echo $job->job_type; ?></span>
+                </div>
+              </div>
+              
+            </li>
+            <br>
+          <?php endforeach; ?>
+     
+        </ul>
+      </div>
+    </section>
