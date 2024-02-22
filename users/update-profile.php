e5eb92916d477d11e934bc238054413e0f54edc2
Twayfrancis <tawaifrancis@gmail.com>
2024-02-22 21:04:01 +0300

    update the functionality to be simpler and easy for the user

--- users/update-profile.php
+++ users/update-profile.php
@@ -219,19 +219,6 @@
   
             </form>
           </div>
-          <!-- <div class="col-lg-5 ml-auto">
-            <div class="p-4 mb-3 bg-white">
-              <p class="mb-0 font-weight-bold">Address</p>
-              <p class="mb-4">203 Fake St. Mountain View, San Francisco, California, USA</p>
-
-              <p class="mb-0 font-weight-bold">Phone</p>
-              <p class="mb-4"><a href="#">+1 232 3235 324</a></p>
-
-              <p class="mb-0 font-weight-bold">Email Address</p>
-              <p class="mb-0"><a href="#">youremail@domain.com</a></p>
-
-            </div>
-          </div> -->
         </div>
       </div>
     </section>
